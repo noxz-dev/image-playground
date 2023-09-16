ff0279f735b862011a9b5ed51b6733f6b9ce3611
Finn Beer <finn.beer@gmail.com>
2023-09-16 23:57:12 +0200

    updated color

--- web/components/Menubar.vue
+++ web/components/Menubar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-white/40 p-2 flex items-center text-white rounded-lg backdrop-blur-md gap-3 ">
+  <div class="bg-black/40 p-2 flex items-center text-white rounded-lg backdrop-blur-md gap-3 ">
     <div class="hover:bg-white/30 rounded-lg p-1 cursor-pointer transition-all" @click="setActiveMode('TRANSLATE')" 
     :class="{
       'bg-white/30': activeMode == 'TRANSLATE',
